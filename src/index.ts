3deb8acb9474d49bd418284ca3709b280a4492eb
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-08-19 13:02:08 +0300

    fix: update import paths and logic in src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
+import { resolvers } from "./resolvers.js";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
