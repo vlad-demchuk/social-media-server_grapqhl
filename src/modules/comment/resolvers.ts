827f24c2be70acf80c539ae0c4f29599f9e62512
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-10-24 15:37:34 +0300

    refactor: streamline comment creation and notification handling

--- src/modules/comment/resolvers.ts
+++ src/modules/comment/resolvers.ts
@@ -1,8 +1,6 @@
 import * as commentService from './service';
-import * as postService from '../post/service';
 import { CommentModule } from './generated-types/module-types';
 import { requireAuth } from '../../utils/authHelpers';
-import { createNotificationPayload, shouldSendNotification, publishNotification } from '../../utils/notificationHelpers';
 import { createSuccessResponse, createErrorResponse } from '../../utils/errorHelpers';
 
 export const resolvers: CommentModule.Resolvers = {
@@ -18,26 +16,15 @@ export const resolvers: CommentModule.Resolvers = {
       const user = requireAuth(context);
 
       try {
-        const comment = await commentService.create({
-          content: args.input.content,
-          userId: user.id,
-          postId: args.input.postId,
-        });
-
-        const commentedPost = await postService.getById(user.id, args.input.postId);
-
-        if (shouldSendNotification(user.id, commentedPost.owner.id)) {
-          const notificationPayload = createNotificationPayload(
-            user,
-            comment.id,
-            'COMMENT',
-            'Your post was commented',
-            'COMMENT',
-            commentedPost.owner.id,
-          );
-
-          await publishNotification(context, notificationPayload);
-        }
+        const comment = await commentService.create(
+          {
+            content: args.input.content,
+            userId: user.id,
+            postId: args.input.postId,
+          },
+          context,
+          user
+        );
 
         return createSuccessResponse('Comment successfully created!', { comment });
       } catch (error) {
--- src/modules/comment/service.ts
+++ src/modules/comment/service.ts
@@ -1,16 +1,41 @@
 import { Comment, CreateCommentInput } from '../../generated-types/graphql';
 import * as commentRepository from './repository';
+import * as postService from '../post/service';
+import { Context, User } from '../../graphql/types';
+import { createNotificationPayload, shouldSendNotification, publishNotification } from '../../utils/notificationHelpers';
 
 export const getByPostId = async (postId: number) => {
   return commentRepository.findByPostId(postId);
 };
 
-export const create = async ({
-  content,
-  userId,
-  postId,
-}: CreateCommentInput & { userId: number }): Promise<Comment> => {
-  return commentRepository.insert({ content, userId, postId });
+export const create = async (
+  {
+    content,
+    userId,
+    postId,
+  }: CreateCommentInput & { userId: number },
+  context: Context,
+  user: User
+): Promise<Comment> => {
+  const comment = await commentRepository.insert({ content, userId, postId });
+
+  // Handle notification logic
+  const commentedPost = await postService.getById(userId, postId);
+
+  if (shouldSendNotification(userId, commentedPost.owner.id)) {
+    const notificationPayload = createNotificationPayload(
+      user,
+      comment.id,
+      'COMMENT',
+      'Your post was commented',
+      'COMMENT',
+      commentedPost.owner.id,
+    );
+
+    await publishNotification(context, notificationPayload);
+  }
+
+  return comment;
 };
 
 export const remove = async (id: number) => {
